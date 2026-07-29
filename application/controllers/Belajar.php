dcaae716325d8956b69abb345ac8828b9b926446
hyoga999 <yogaharimas@gamil.com>
2026-07-29 22:45:18 +0700

    coba update 270726

--- application/controllers/Belajar.php
+++ application/controllers/Belajar.php
@@ -19,7 +19,7 @@ class Belajar extends CI_Controller
 	function List()
 	{
 		$data = array(
-			'judul' => "Belajar"
+			'judul' => "Belajar Hukum"
 		);
 		$this->load->view('header', $data);
 		$this->load->view('Belajar/v_belajar', $data);
